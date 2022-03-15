d9fb9b2800f2c4cba04242d90adea5b43b4368ea
joaonicholasfigueiredo <joaonicholasfigueiredo@gmail.com>
2022-03-14 22:37:03 -0300

    Commit inicial
    Primeira tela concluída.

---
+++ Proj_votacao_JOAO_NICHOLAS/src/senai/com/br/funcionalidades/Registro_candidato_nicholas.java
@@ -0,0 +1,363 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package senai.com.br.funcionalidades;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class Registro_candidato_nicholas extends javax.swing.JFrame {
+
+    //Inicio instanciação métodos estaticos
+    static Registro_candidato_nicholas cadastroDeCandidatos = new Registro_candidato_nicholas();
+    public static ArrayList<RegistroCandidato> listaDeCandidatos = new ArrayList<>();
+    //Fim instanciação de métodos estaticos
+
+    public class RegistroCandidato {
+
+        ///////////////////////////////////////
+        ///////////////////////////////////////
+        //INICIO ATRIBUTOS, GETTERS E SETTERS//
+        ///////////////////////////////////////
+        ///////////////////////////////////////
+        //Atributos
+        private String nomeCandidato = "";
+        private int numeroPartido = 0;
+        private int numeroCanditato = 0;
+        private int array[] = null;
+
+        public RegistroCandidato() {
+
+        }
+
+        public RegistroCandidato(String nomeCandidato, int numeroPartido, int numeroCandidato) {
+            this.nomeCandidato = nomeCandidato;
+            this.numeroPartido = numeroPartido;
+            this.numeroCanditato = numeroCandidato;
+        }
+
+        public String getNomeCandidato() {
+            return nomeCandidato;
+        }
+
+        public void setNomeCandidato(String nomeCandidato) throws Exception {
+            this.nomeCandidato = nomeCandidato;
+        }
+
+        public int getNumeroPartido() {
+            return numeroPartido;
+        }
+
+        public void setNumeroPartido(int numeroPartido) throws Exception {
+            this.numeroPartido = numeroPartido;
+        }
+
+        public int getNumeroCanditato() {
+            return numeroCanditato;
+        }
+
+        public void setNumeroCanditato(int numeroCanditato) throws Exception {
+            this.numeroCanditato = numeroCanditato;
+        }
+        //////////////////////////////////////
+        //////////////////////////////////////
+        ///FIM ATRIBUTOS, GETTERS E SETTERS///
+        //////////////////////////////////////
+        ////////////////////////////////////// 
+
+        //
+        public String[] getDadosComoVetorString() {
+            return new String[]{nomeCandidato, String.valueOf(numeroPartido), String.valueOf(numeroCanditato)};
+        }
+    }
+
+    public void atualizarTabelaDadosDeCandidato() {
+        DefaultTableModel model = (DefaultTableModel) jTableListaDeCandidatosCadastrados.getModel();
+
+        //limpar tela
+        for (int i = model.getRowCount()-1; i >= 0; i--) {
+            model.removeRow(0);
+        }
+
+        //iterar na lista de candidatos cadastrados
+        for (RegistroCandidato registroCandidato : listaDeCandidatos) {
+            model.addRow(registroCandidato.getDadosComoVetorString());
+        }
+
+    }
+
+    public Registro_candidato_nicholas() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+    //Inicio método de limpar tela
+    public void limparTela() {
+        jTextFieldNomeDoCandidato.setText("");
+        jTextFieldNumeroEleitoralDoCandidato.setText("");
+        jTextFieldPartidoDoCandidato.setText("");
+    }
+    //Fim método de limpar tela
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextFieldNomeDoCandidato = new javax.swing.JTextField();
+        jTextFieldPartidoDoCandidato = new javax.swing.JTextField();
+        jTextFieldNumeroEleitoralDoCandidato = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTableListaDeCandidatosCadastrados = new javax.swing.JTable();
+        jButtonCadastrarCandidato = new javax.swing.JButton();
+        jButtonLimparCampos = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(0, 102, 102));
+
+        jPanel2.setBackground(new java.awt.Color(204, 153, 0));
+
+        jLabel1.setFont(new java.awt.Font("Iosevka Fixed", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("Bem - vindo ao sistema Eleitoral 2022!!");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(138, 138, 138)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addContainerGap(30, Short.MAX_VALUE))
+        );
+
+        jLabel2.setFont(new java.awt.Font("Iosevka Fixed", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Cadastre seu Candidato");
+
+        jLabel3.setFont(new java.awt.Font("Iosevka Fixed", 1, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("Nome do Candidato");
+
+        jLabel4.setFont(new java.awt.Font("Iosevka Fixed", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("Numero do Partido");
+
+        jLabel5.setFont(new java.awt.Font("Iosevka Fixed", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel5.setText("Numero Eleitoral");
+
+        jTableListaDeCandidatosCadastrados.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Nome do Candidato", "Numero do Partido", "Numero Eleitoral"
+            }
+        ));
+        jScrollPane1.setViewportView(jTableListaDeCandidatosCadastrados);
+
+        jButtonCadastrarCandidato.setText("Cadastrar");
+        jButtonCadastrarCandidato.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonCadastrarCandidatoActionPerformed(evt);
+            }
+        });
+
+        jButtonLimparCampos.setText("Limpar");
+        jButtonLimparCampos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonLimparCamposActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Ir para Votação");
+
+        jButton3.setText("Ir para Apuração");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextFieldNomeDoCandidato, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextFieldPartidoDoCandidato)
+                            .addComponent(jTextFieldNumeroEleitoralDoCandidato)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButtonCadastrarCandidato, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButtonLimparCampos, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(14, 14, 14))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(27, 27, 27)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(jTextFieldNomeDoCandidato, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(jTextFieldPartidoDoCandidato, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(jTextFieldNumeroEleitoralDoCandidato, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButtonCadastrarCandidato, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+                            .addComponent(jButtonLimparCampos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(31, 31, 31)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonLimparCamposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLimparCamposActionPerformed
+        // TODO add your handling code here:
+        limparTela();
+    }//GEN-LAST:event_jButtonLimparCamposActionPerformed
+
+    private void jButtonCadastrarCandidatoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCadastrarCandidatoActionPerformed
+        // TODO add your handling code here:
+        try {
+
+            //Instanciar obj tabela
+            //Tabela de resultado de candidatos cadastrados
+            RegistroCandidato regCandidato = new RegistroCandidato();
+
+            regCandidato.setNomeCandidato(jTextFieldNomeDoCandidato.getText());
+            regCandidato.setNumeroPartido(Integer.parseInt(jTextFieldPartidoDoCandidato.getText()));
+            regCandidato.setNumeroCanditato(Integer.parseInt(jTextFieldNumeroEleitoralDoCandidato.getText()));
+            listaDeCandidatos.add(regCandidato);
+
+            atualizarTabelaDadosDeCandidato();
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(this, erro.getMessage());
+        }
+    }//GEN-LAST:event_jButtonCadastrarCandidatoActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Registro_candidato_nicholas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Registro_candidato_nicholas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Registro_candidato_nicholas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Registro_candidato_nicholas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Registro_candidato_nicholas().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButtonCadastrarCandidato;
+    private javax.swing.JButton jButtonLimparCampos;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTableListaDeCandidatosCadastrados;
+    private javax.swing.JTextField jTextFieldNomeDoCandidato;
+    private javax.swing.JTextField jTextFieldNumeroEleitoralDoCandidato;
+    private javax.swing.JTextField jTextFieldPartidoDoCandidato;
+    // End of variables declaration//GEN-END:variables
+}
